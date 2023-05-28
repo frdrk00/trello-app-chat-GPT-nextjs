c535dfe8685ecd6d195e62b8efa3dcf75f083e2b
frdrk00 <masterkooo@gmail.com>
2023-05-28 16:26:22 +0200

    Implementing To-Do Data

--- lib/getTodosGroupedByColumn.ts
+++ lib/getTodosGroupedByColumn.ts
@@ -2,9 +2,33 @@ import { databases } from "@/appwrite";
 
 export const getTodosGroupedByColumn = async () => {
   const data = await databases.listDocuments(
-    process.env.NEXT_PUBLIC_DATASE_ID!,
+    process.env.NEXT_PUBLIC_DATABASE_ID!,
     process.env.NEXT_PUBLIC_TODOS_COLLECTION_ID!
   );
 
-  console.log(data);
+  const todos = data.documents;
+
+  console.log(todos);
+
+  const columns = todos.reduce((acc, todo) => {
+    if (!acc.get(todo.status)) {
+      acc.set(todo.status, {
+        id: todo.status,
+        todos: [],
+      });
+    }
+
+    acc.get(todo.status)!.todos.push({
+      $id: todo.$id,
+      $createdAt: todo.$createdAt,
+      title: todo.title,
+      status: todo.status,
+      // get the image if it exists in the todo
+      ...(todo.image && { image: JSON.parse(todo.image) }),
+    });
+
+    return acc;
+  }, new Map<TypedColumn, Column>());
+
+  console.log(columns);
 };
