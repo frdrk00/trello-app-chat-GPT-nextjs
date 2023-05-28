ad2709aa2d9f0be9af62eecc055297cc08410163
frdrk00 <masterkooo@gmail.com>
2023-05-28 22:23:34 +0200

    Column Component

---
+++ components/ToDoCard.tsx
@@ -0,0 +1,45 @@
+"use client";
+
+import { XCircleIcon } from "@heroicons/react/24/solid";
+import { FC } from "react";
+import {
+  DraggableProvidedDragHandleProps,
+  DraggableProvidedDraggableProps,
+} from "react-beautiful-dnd";
+
+interface ToDoCardProps {
+  todo: Todo;
+  index: number;
+  id: TypedColumn;
+  innerRef: (element: HTMLElement | null) => void;
+  draggableProps: DraggableProvidedDraggableProps;
+  dragHandleProps: DraggableProvidedDragHandleProps | null | undefined;
+}
+
+const ToDoCard: FC<ToDoCardProps> = ({
+  todo,
+  index,
+  id,
+  innerRef,
+  dragHandleProps,
+  draggableProps,
+}) => {
+  return (
+    <div
+      className="bg-white rounded-md space-y-2 drop-shadow-md"
+      {...draggableProps}
+      {...dragHandleProps}
+      ref={innerRef}
+    >
+      <div className="flex justify-between items-center p-5">
+        <p>{todo.title}</p>
+        <button className="text-red-500 hover:text-red-600">
+          <XCircleIcon className="ml-5 h-8 w-8" />
+        </button>
+      </div>
+      {/* Image */}
+    </div>
+  );
+};
+
+export default ToDoCard;
--- lib/getTodosGroupedByColumn.ts
+++ lib/getTodosGroupedByColumn.ts
@@ -8,8 +8,6 @@ export const getTodosGroupedByColumn = async () => {
 
   const todos = data.documents;
 
-  console.log(todos);
-
   const columns = todos.reduce((acc, todo) => {
     if (!acc.get(todo.status)) {
       acc.set(todo.status, {
@@ -30,5 +28,27 @@ export const getTodosGroupedByColumn = async () => {
     return acc;
   }, new Map<TypedColumn, Column>());
 
-  console.log(columns);
+  // if columns doesn't have inprogress, todo and done, add them with empty todos
+  const columnTypes: TypedColumn[] = ["todo", "inprogress", "done"];
+  for (const columnType of columnTypes) {
+    if (!columns.get(columnType)) {
+      columns.set(columnType, {
+        id: columnType,
+        todos: [],
+      });
+    }
+  }
+
+  // sort columns by columnTypes
+  const sortedColumns = new Map(
+    Array.from(columns.entries()).sort(
+      (a, b) => columnTypes.indexOf(a[0]) - columnTypes.indexOf(b[0])
+    )
+  );
+
+  const board: Board = {
+    columns: sortedColumns,
+  };
+
+  return board;
 };
